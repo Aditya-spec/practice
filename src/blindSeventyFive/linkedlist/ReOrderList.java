3900b388e006ef414a6cdb42ec46a53869534a2f
adityasingh <adityaalp1996@gmail.com>
2025-01-27 11:04:17 +0530

    Added code for linked list reorder

---
+++ src/blindSeventyFive/linkedlist/ReOrderList.java
@@ -0,0 +1,54 @@
+package blindSeventyFive.linkedlist;
+
+public class ReOrderList {
+    public Node reOrderList(Node node){
+        Node middle = getMiddle(node);
+        Node second = reverseList(middle);
+        Node head = reOrderList(node, second);
+        return node;
+    }
+
+    private Node reOrderList(Node first, Node second) {
+
+        Node c1 = first;
+        Node c2 = second;
+        Node f1 = null;
+        Node f2 = null;
+
+        while(c1 != null && c2 != null){
+            f1 = c1.next;
+            f2 = c2.next;
+
+            c1.next = c2;
+            c2.next = f1;
+
+            c1 = f1;
+            c2 = f2;
+        }
+
+        return c1;
+    }
+
+    private Node reverseList(Node head) {
+        Node next = head;
+        Node prev = null;
+        Node curr = head;
+        while (curr.next != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+
+    private Node getMiddle(Node node) {
+        Node fastP = node;
+        Node slowP = node;
+        while (fastP != null || fastP.next.next != null ){
+            slowP = slowP.next;
+            fastP = fastP.next.next;
+        }
+        return slowP.next;
+    }
+}
